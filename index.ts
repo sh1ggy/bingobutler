5af5b364b3937fc0d35710b38924f80f65ca84d9
anhad <anhadrs@gmail.com>
2021-09-12 01:46:40 +1000

    send change

--- index.ts
+++ index.ts
@@ -25,7 +25,63 @@ async function main() {
     cors: { origin: "*", }
   });
   app.use(cors());
+  app.get('/login', (req, res) => {
+    return res.redirect(
+      'https://discordapp.com/api/oauth2/authorize' +
+      '?client_id=' + process.env.CLIENT_ID +
+      '&redirect_uri=' +
+      encodeURIComponent(callback_url) +
+      '&response_type=code' +
+      '&scope=identify%20email'
+    );
+  })
 
+
+
+
+
+  app.get('/callback', async (req, res) => {
+    let code = req.query.code;
+    console.log(code);
+    const params = new URLSearchParams();
+    params.append("client_id", process.env.CLIENT_ID)
+    params.append("client_secret", process.env.CLIENT_SECRET)
+    params.append("grant_type", 'authorization_code')
+    params.append("redirect_uri", callback_url)
+    params.append("scope", 'identify email')
+    params.append("code", code.toString())
+    const url = 'https://discordapp.com/api/v8/oauth2/token';
+    const options = {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
+      },
+    }
+    let data;
+    try {
+      const result = await axios.post(url, params, options);
+      console.log(result.data);
+      data = result.data;
+    } catch (error) {
+      console.log(error.message);
+      res.status(400);
+      return res.send(error.message);
+    }
+
+    const resourceURL = 'https://discordapp.com/api/v8/users/@me'
+    const resOptions = {
+      headers: {
+        Authorization: 'Bearer ' + data.access_token
+      },
+    }
+    let user = await axios.get(resourceURL, resOptions)
+    console.log(user.data)
+    const payload = { ...user.data, rt: data.refresh_token }
+
+    await db.collection("users").insertOne(payload);
+    const backToLoginURL = 'http://localhost:3000/auth?rt=' + data.refresh_token;
+    res.redirect(backToLoginURL);
+  })
 
   io.on("connection", (socket) => {
     console.log('a user connected');
@@ -48,76 +104,10 @@ async function main() {
     });
   });
 
-  app.get('/login', (req, res) => {
-    return res.redirect(
-      'https://discordapp.com/api/oauth2/authorize' +
-      '?client_id=' + process.env.CLIENT_ID +
-      '&redirect_uri=' +
-      encodeURIComponent(callback_url) +
-      '&response_type=code' +
-      '&scope=identify%20email'
-    );
+
+  app.all('*', (req, res) => {
+    return handle(req, res)
   })
-
-  app.get('/callback', async (req, res) => {
-    let code = req.query.code;
-    console.log(code);
-    const params = new URLSearchParams();
-    params.append("client_id", process.env.CLIENT_ID)
-    params.append("client_secret", process.env.CLIENT_SECRET)
-    params.append("grant_type", 'authorization_code')
-    params.append("redirect_uri", callback_url)
-    params.append("scope", 'identify email')
-    params.append("code", code.toString())
-
-    // const body = {
-    //   client_id: process.env.CLIENT_ID,
-    //   client_secret: process.env.CLIENT_SECRET,
-    //   grant_type: 'authorization_code',
-    //   redirect_uri: callback_url,
-    //   scope: 'identify email',
-    //   code
-    // }
-    // console.log(body)
-    const url = 'https://discordapp.com/api/v8/oauth2/token';
-    const options = {
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json'
-      },
-    }
-    let data;
-    try {
-      const result = await axios.post(url, params, options);
-      console.log(result.data);
-      // a = result.data.access_token;
-      // rt = result.data.refresh_token;
-      data = result.data;
-    } catch (error) {
-      console.log(error.message);
-      res.status(400);
-      return res.send(error.message);
-    }
-
-    const resourceURL = 'https://discordapp.com/api/v8/users/@me'
-    const resOptions = {
-      headers: {
-        Authorization: 'Bearer ' + data.access_token
-      },
-    }
-    let user = await axios.get(resourceURL, resOptions)
-    console.log(user.data)
-    const payload = { ...user.data, rt: data.refresh_token }
-
-    // res.send(JSON.stringify(payload));
-    await db.collection("users").insertOne(payload);
-
-    const backToLoginURL = 'http://localhost:3000/auth?rt=' + data.refresh_token;
-    // db, localstorage, websockets
-    res.redirect(backToLoginURL);
-  })
-
-
 
 
   const PORT = process.env.PORT || 3001;
