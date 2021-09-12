fd83a872de0989ce30ad9acc7eb7933a6d43f74c
anhad <anhadrs@gmail.com>
2021-09-12 13:05:02 +1000

    test

--- pages/game/[id].tsx
+++ pages/game/[id].tsx
@@ -75,17 +75,14 @@ const Home: NextPage = ({ multiGame, game }) => {
   }, []);
 
   function onLock(index) {
-    // let tempCompleted = [...completed];
-    // tempCompleted[index] = !tempCompleted[index];
-    // setCompleted(tempCompleted)
+
     console.log(completed);
     if (user.id == completed[index]) {
       io.current.emit('undo', { rt: user.rt, index, gameId: game._id });
       return;
     }
+    if (completed[index] !=-1) return;
     io.current.emit('done', { rt: user.rt, index, gameId: game._id });
-
-    return;
   }
 
   return (
